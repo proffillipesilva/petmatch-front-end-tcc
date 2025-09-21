4f967760d74034e63a3939262c70cfcde4884010
henriquecorrea-fiec <henrique38292@fiec.edu.br>
2025-09-21 17:06:07 -0300

    nFOI?

--- src/features/splash/SplashScreen.js
+++ src/features/splash/SplashScreen.js
@@ -1,5 +1,23 @@
+import React from 'react';
+import AuthImg from './assets/Auth.png';
+import LoginScreen from './components/LoginScreen'; 
 
+const SplashScreen = ({ onSwitchToRegister, onLoginSuccess }) => {
+  return (
+    <div
+      className="relative flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${AuthImg})`
+      }}
+    >
+      <div className="relative z-10 w-full max-w-sm">
+        <LoginScreen 
+          onSwitchToRegister={onSwitchToRegister}
+          onLoginSuccess={onLoginSuccess}
+        />
+      </div>
+    </div>
+  );
+};
 
-
-
-TEM Q FAZER AINDA
+export default SplashScreen;

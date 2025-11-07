714bbbd2d85e04aeca70271cba48d2d479f25fcd
henriquecorrea-fiec <henrique38292@fiec.edu.br>
2025-11-06 21:36:52 -0300

    alterações com Fillipe sobre o android

--- src/shared/utils/publicApi.js
+++ src/shared/utils/publicApi.js
@@ -6,7 +6,7 @@ const { showLoading, showSuccess, showError, closeModal } = useStatusModalStore.
 
 const publicApi = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL 
-});
+}); 
 
 // -----------------------------------------------------------------
 // Request Interceptor (APENAS Modal)
